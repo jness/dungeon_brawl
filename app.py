1130dc77ec34ee077c9648a4fcc5bc369958610f
Jeffrey Ness <jness@nessy.info>
2018-08-15 15:32:57 -0500

    Adding spells

--- app.py
+++ app.py
@@ -14,9 +14,10 @@ app = Flask(__name__)
 # create our Mongo connection
 mongo = MongoClient('mongo', username='root', password='password')
 
-# connect to collection within our database
+# connect to collections within our database
 database = mongo.dungeon_brawl
-collection = database.monsters
+monster_collection = database.monsters
+spell_collection = database.spells
 
 @app.route('/', methods=['GET'])
 def main():
@@ -25,13 +26,29 @@ def main():
     """
 
     # get all monsters
-    results = collection.find({})
+    results = monster_collection.find({})
 
     # perform column sort
     if 'sort' in request.args:
         results = results.sort(request.args['sort'])
 
     return render_template('monsters.html', monsters=results)
+
+
+@app.route('/spells', methods=['GET'])
+def spells():
+    """
+    Main spell page
+    """
+
+    # get all monsters
+    results = spell_collection.find({})
+
+    # perform column sort
+    if 'sort' in request.args:
+        results = results.sort(request.args['sort'])
+
+    return render_template('spells.html', spells=results)
 
 
 @app.route('/search', methods=['GET'])
@@ -56,10 +73,10 @@ def monsters():
 
         # perform search
         if search_field == 'challenge_rating':
-            results = collection.find({search_field: search_text})
+            results = monster_collection.find({search_field: search_text})
         else:
             regex = re.compile(search_text, re.IGNORECASE)
-            results = collection.find({search_field: regex})
+            results = monster_collection.find({search_field: regex})
 
         # perform column sort
         if 'sort' in request.args:
@@ -82,10 +99,27 @@ def monster(slug_name):
     """
 
     # find monsters matching regex
-    results = collection.find_one({'slug_name': slug_name})
+    results = monster_collection.find_one({'slug_name': slug_name})
+
 
     if results:
-        return render_template('monster.html', monster=results)
+        return render_template(
+            'monster.html', monster=results, spells=spells)
+
+    abort(404)
+
+
+@app.route('/spell/<slug_name>', methods=['GET'])
+def spell(slug_name):
+    """
+    Return a spell by name
+    """
+
+    # find monsters matching regex
+    results = spell_collection.find_one({'slug_name': slug_name})
+
+    if results:
+        return render_template('spell.html', spell=results)
 
     abort(404)
 
@@ -103,7 +137,7 @@ def brawl_add():
         slug_name = request.args['slug_name']
 
         # make sure the slug_name is in database
-        monster = collection.find_one({'slug_name': slug_name})
+        monster = monster_collection.find_one({'slug_name': slug_name})
         if monster:
 
             # add monster to monster json list
--- populate.py
+++ populate.py
@@ -10,7 +10,8 @@ mongo = MongoClient('mongo', username='root', password='password')
 
 # connect to collection within our database
 database = mongo.dungeon_brawl
-collection = database.monsters
+monsters = database.monsters
+spells = database.spells
 
 # iterate over all monster files
 for file_path in sorted(glob.glob('monsters/*.json')):
@@ -21,10 +22,27 @@ for file_path in sorted(glob.glob('monsters/*.json')):
 
         # check if document exists in mongo
         slug_name = document['slug_name']
-        if collection.find_one({'slug_name': slug_name}):
+        if monsters.find_one({'slug_name': slug_name}):
             print('Document %s already exists' % slug_name)
             continue
 
         # import monster into mongo
         print('Inserting %s' % slug_name)
-        collection.insert_one(document)
+        monsters.insert_one(document)
+
+# iterate over all spells files
+for file_path in sorted(glob.glob('spells/*.json')):
+    with open(file_path) as f:
+
+        # load json file into python dictionary
+        document = json.loads(f.read())
+
+        # check if document exists in mongo
+        slug_name = document['slug_name']
+        if spells.find_one({'slug_name': slug_name}):
+            print('Document %s already exists' % slug_name)
+            continue
+
+        # import monster into mongo
+        print('Inserting %s' % slug_name)
+        spells.insert_one(document)
