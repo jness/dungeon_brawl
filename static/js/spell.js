6c53d57f414b272ac83bf39c3047a85d6bba7326
Jeffrey Ness <jness@nessy.info>
2018-08-20 18:34:20 -0500

    No longer strong text

--- static/js/spell.js
+++ static/js/spell.js
@@ -1,26 +1,5 @@
 
-// This function will bold the text within description blocks
-function highlight_description() {
-
-  paragraphs = document.getElementsByClassName("description");
-
-  for (i = 0; i < paragraphs.length; i++) {
-
-    paragraphs[i].innerHTML = paragraphs[i].innerHTML.replace(
-      "At Higher Levels.",
-      "<strong>At Higher Levels.</strong>"
-    );
-
-    paragraphs[i].innerHTML = paragraphs[i].innerHTML.replace(
-      "This spell's damage increases when you reach higher levels",
-      "<strong>This spell's damage increases when you reach higher levels</strong>"
-    );
-
-  }
-
-}
-
 // execute on page load
 $(window).on('load',function(){
-    highlight_description();
+    // do a thing
 });
