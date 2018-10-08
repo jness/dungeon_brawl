be4c5968f8d3c68ead0afa7a35282d8855e5f6a4
Jeffrey Ness <jness@nessy.info>
2018-10-08 14:42:08 -0500

    Cleanup dice modal a bit

--- static/js/base.js
+++ static/js/base.js
@@ -1,3 +1,4 @@
+counter = 0;
 total = 0;
 
 function clear_dice() {
@@ -6,13 +7,15 @@ function clear_dice() {
   dice_total = document.getElementById("dice_total");
   dice_total.innerHTML = "";
   total = 0;
+  counter = 0;
 }
 
 function roll_dice(number) {
   roll = Math.floor(Math.random() * number) + 1;
   total += roll;
+  counter += 1;
   dice_result = document.getElementById("dice_result");
-  dice_result.innerHTML = dice_result.innerHTML + "<strong>d" + number + "</strong> rolled a <strong>" + roll + "</strong><br>";
+  dice_result.innerHTML = dice_result.innerHTML + "<small>[" + counter + "]</small> <strong>d" + number + "</strong> rolled a <strong>" + roll + "</strong><br>";
   dice_total = document.getElementById("dice_total");
-  dice_total.innerHTML = "<hr><strong>Total</strong>: " + total;
+  dice_total.innerHTML = "<br><strong>Total</strong>: " + total;
 }
