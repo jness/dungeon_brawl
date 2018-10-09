f004100318832a648f46326865372978b350ca8a
Jeffrey Ness <jness@nessy.info>
2018-10-09 12:32:09 -0500

    Allow encounters yaml to contain html

--- scripts/purge.py
+++ scripts/purge.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import sys
+
 from pymongo import MongoClient
 
 
@@ -9,12 +11,26 @@ mongo = MongoClient('mongo', username='root', password='password')
 # connect to collection within our database
 database = mongo.dungeon_brawl
 
-# get a list of existing collections
-collections = database.list_collection_names()
 
-# drop each collection in the dungeon_brawl database
-for collection in collections:
+try:
 
-    print('Dropping collection %s' % collection)
-    c = getattr(database, collection)
+    # if argv was passed delete single collection
+    collection_name = sys.argv[1]
+
+    print('Dropping collection %s' % collection_name)
+    c = getattr(database, collection_name)
     c.drop()
+
+except IndexError:
+
+    # get a list of existing collections
+    collections = database.list_collection_names()
+
+    # drop each collection in the dungeon_brawl database
+    for collection in collections:
+
+
+
+        print('Dropping collection %s' % collection)
+        c = getattr(database, collection)
+        c.drop()
